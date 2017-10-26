d9df402db247e1c1b70ef08f3c4dbc039f0437c3
Andrea Parodi <andrea@parro.it>
2017-10-26 15:11:54 +0200

    refactor

--- index.js
+++ index.js
@@ -1,3 +1,116 @@
-export default function templateLiteralTags() {
-  return 42;
+import justified from "justified";
+
+class TagStart {
+  constructor(tagName, render) {
+    this.tagName = tagName;
+    this.render = render;
+  }
+}
+class TagEnd {
+  constructor(tagName) {
+    this.tagName = tagName;
+  }
+}
+
+const mkTag = (name, render) => ({
+  start: new TagStart(name, render),
+  end: new TagEnd(name)
+});
+
+export const upper = mkTag("upper", content =>
+  content
+    .trim()
+    .replace(/[\n\s]+/g, " ")
+    .toUpperCase()
+);
+
+const justifyToWidth = (width, content, before) => {
+  const flatted = content
+    .replace(/\n\n+/g, "§")
+    .replace(/\s+/g, " ")
+    .replace(/§/g, "\n\n")
+    .trim();
+  const linePosition = before.match(/\n(.+)$/) || before.match(/^(.+)$/);
+  const tagIndent = linePosition[1].length;
+  const indentation = new Array(tagIndent).fill(" ").join("");
+
+  const ret = justified(flatted, {
+    width,
+    indent: "§"
+  })
+    .trim()
+    .replace(/§/g, indentation);
+  return ret.trim();
+};
+
+export const justify = mkTag("justify", (content, before) => {
+  const firstLine = content.split("\n").filter(l => l.trim() !== "")[0];
+  const width = firstLine.trim().length;
+  return justifyToWidth(width, content, before);
+});
+
+export const justifyWidth = width =>
+  mkTag("justify", (content, before) => {
+    return justifyToWidth(width, content, before);
+  });
+
+function handleTagStart({ i, value, strings, values, result }) {
+  const tagName = value.tagName;
+  const otherStrings = [];
+  const otherValues = [];
+  let j = i + 1;
+  for (; j < strings.length; j++) {
+    otherStrings.push(strings[j]);
+    if (j < values.length) {
+      const otherValue = values[j];
+      if (otherValue instanceof TagEnd && otherValue.tagName === tagName) {
+        break;
+      }
+      otherValues.push(otherValue);
+    }
+  }
+  i = j;
+  if (otherStrings.length !== 0 || otherValues.length !== 0) {
+    const tagContent = interpolate(otherStrings, otherValues);
+    const rendered = value.render(tagContent, result);
+    return { tagValue: rendered.trim(), idx: i };
+  }
+
+  return { tagValue: value.render("", result).trim(), idx: i };
+}
+
+export function interpolate(strings, values) {
+  let result = "";
+  for (let i = 0; i < strings.length; i++) {
+    result += strings[i];
+
+    if (i < values.length) {
+      const value = values[i];
+      if (value instanceof TagStart) {
+        const { tagValue, idx } = handleTagStart({
+          i,
+          value,
+          strings,
+          values,
+          result
+        });
+        result += tagValue;
+        i = idx;
+      } else {
+        result += value;
+      }
+    }
+  }
+  return result;
+}
+
+export function report(strings, ...values) {
+  const result = interpolate(strings, values);
+  const firstLine = result.split("\n").filter(l => l.trim() !== "")[0];
+  const firstLineIndentedBy = firstLine.match(/^(\s*)/)[1].length;
+  return (
+    result
+      .replace(new RegExp(`\\n[ \\t]{${firstLineIndentedBy}}`, "g"), "\n")
+      .trim() + "\n"
+  );
 }
--- test.js
+++ test.js
@@ -1,6 +1,62 @@
 import test from "ava";
-import templateLiteralTags from ".";
+import { report, upper, justify } from ".";
 
-test("exports a function", t => {
-  t.is(typeof templateLiteralTags, "function");
+test("simple", t => {
+  t.is(report`a${42}c`, "a42c\n");
+});
+
+test("upper", t => {
+  const res = report`
+    abcd
+    ${upper.start}
+      abcd
+    ${upper.end}
+  `;
+  t.is(res, `abcd\nABCD\n`);
+});
+
+test("justify", t => {
+  const res = report`
+    This option shall be supported if the implementation
+    supports the User Portability Utilities option. It
+    shall cause the shell to notify the user asynchronously
+
+    ${justify.start}
+       of background job completions. The following
+       message is written to standard error: "[%d]%c %s%s\n"
+       , <job-number>, <current>, <status>, <job-name> where
+       the fields shall be as follows: <current> The character
+        '+' identifies the job that would be used as a default
+         for the fg or bg utilities; this job can also be
+          specified using the job_id "%+" or "%%". The character
+           '-' identifies the job that would become the default
+    ${justify.end}
+  `;
+  t.is(
+    res,
+    `This option shall be supported if the implementation
+supports the User Portability Utilities option. It
+shall cause the shell to notify the user asynchronously
+
+of  background job completions. The following
+message  is  written    to   standard  error:
+"[%d]%c  %s%s  "  , <job-number>,  <current>,
+<status>,  <job-name> where  the fields shall
+be as follows: <current>   The character  '+'
+identifies  the job that would be  used  as a
+default for the fg  or bg utilities; this job
+can also be  specified using the  job_id "%+"
+or   "%%".  The  character '-' identifies the
+job that would become the default
+`
+  );
+});
+
+test("auto unindent", t => {
+  const res = report`
+       a
+        b
+         c
+       `;
+  t.is(res, "a\n b\n  c\n");
 });
